3f09c6b1f20a6e1016a4bd003e9a743b0258178f
sandsq <116196797+sandsq@users.noreply.github.com>
2024-04-26 12:18:07 -0400

    performance: basic flamegraph as baseline for changing scoring function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
-use alc::{objective::scoring::SimpleScoreFunction, optimizer::LayoutOptimizer};
+use alc::{objective::scoring::{AdvancedScoreFunction, SimpleScoreFunction}, optimizer::LayoutOptimizer};
 
 
 
 
 fn main() {
 	// cargo flamegraph --bin=alc --palette=rust --output=performance/0_1_1.svg
-	let mut lo = LayoutOptimizer::<4, 12, SimpleScoreFunction>::default();
+	let mut lo = LayoutOptimizer::<4, 12, AdvancedScoreFunction>::default();
 	// let mut config = LayoutOptimizerConfig::default();
-	lo.config.generation_count = 20;
-	lo.config.population_size = 1000;
+	lo.config.generation_count = 50;
+	lo.config.population_size = 100;
 	lo.config.keycode_options.include_numbers = true;
 	println!("initial valid keycodes {:?}", lo.config.valid_keycodes);
 	let mut rng = ChaCha8Rng::seed_from_u64(1);
